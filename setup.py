ddc2827caca970c6df156e32deff2691e09d2c60
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-03-04 18:15:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 """Setup script."""
 
+from os import path
 from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, '..', 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='zoom_korokesu',
+    version='1.0.0',
     packages=find_packages(exclude=['tests']),
 
     install_requires=['pyserial'],
+
+    extra_requires={
+        'test': ['pytest'],
+    },
+
+    author='Pollen Robotics',
+    author_email='contact@pollen-robotics.com',
+    url='https://github.com/pollen-robotics/zoom_korokesu',
+
+    description="Python Package for controlling the zoom of Kurokesu's camera.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
