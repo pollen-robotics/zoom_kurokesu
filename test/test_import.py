87576abe817ad9077268d70ce270a6d985e5cbd4
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-03-04 19:36:35 +0100

    Prepare publication on PyPi. (#4)
    * Update setup.py
    * Update notebook with new API.

--- test/test_import.py
+++ test/test_import.py
@@ -1,5 +1,5 @@
 def test_module_import():
-    import zoom_korokesu
+    import zoom_kurokesu
 
 def test_controller_import():
-    from zoom_korokesu import ZoomController
+    from zoom_kurokesu import ZoomController
