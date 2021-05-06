8c376add7c52fdd6876b3abb83884f9cec1e0c2d
marjoriePaillet <marjoriepaillet@live.fr>
2021-05-06 14:28:26 +0200

    Add two cameras control methods.

--- zoom_kurokesu/zoom_piloting.py
+++ zoom_kurokesu/zoom_piloting.py
@@ -62,9 +62,67 @@ class ZoomController:
         zoom, focus = self.zoom_pos[side][zoom_level].values()
         self._send_custom_command(side, zoom, focus)
 
+    def send_zoom_command_two_cameras(self, left_zoom: str, right_zoom: str) -> None:
+        """Send a zoom command to both cameras in a same time.
+
+        Given the zoom level required to each cameras,
+        produce the corresponding G-code and send it over the serial port.
+
+        Args:
+            left_zoom: either 'in', 'inter' or 'out'. 'in' level for far
+                 objects, 'out' for close objects, 'inter' is in between
+                 'in' and 'out' levels
+            right_zoom: either 'in', 'inter' or 'out'. 'in' level for far
+                 objects, 'out' for close objects, 'inter' is in between
+                 'in' and 'out' levels
+        """
+        val_left = self.zoom_pos["left"][left_zoom]["zoom"]
+        val_right = self.zoom_pos["right"][right_zoom]["zoom"]
+        self.send_custom_zoom_two_cameras(val_left, val_right)
+
     def _send_custom_command(self, side: str, zoom: int, focus: int):
         mot = self.motors[self.connector[side]]
         command = f'G1 {mot["zoom"]}{zoom} {mot["focus"]}{focus} F{self.speed}'
+        self.ser.write(bytes(command + '\n', 'utf8'))
+        _ = self.ser.readline()
+
+    def send_custom_zoom_two_cameras(self, left_zoom: int, right_zoom: int):
+        """Send custom zoom values to both cameras.
+
+        Given left and right zoom value,
+        produce the corresponding G-code and send it over the serial port.
+        motors will move at once
+
+        Args:
+            left_zoom: int between 0 and 600
+            right_zoom: int between 0 and 600
+        """
+        if not (0 <= left_zoom <= 600 or 0 <= right_zoom <= 600):
+            raise ValueError('Zoom value must be between 0 and 600.')
+
+        mot_l = self.motors[self.connector['left']]
+        mot_r = self.motors[self.connector['right']]
+        command = f'G1 {mot_l["zoom"]}{left_zoom} {mot_r["zoom"]}{right_zoom} F{self.speed}'
+        self.ser.write(bytes(command + '\n', 'utf8'))
+        _ = self.ser.readline()
+
+    def send_custom_focus_two_cameras(self, left_focus: int, right_focus: int):
+        """Send custom focus values to both cameras.
+
+        Given left and right focus value,
+        produce the corresponding G-code and send it over the serial port.
+        motors will move at once
+
+        Args:
+            left_focus: int between 0 and 600
+            right_focus: int between 0 and 600
+        """
+        if not (0 <= left_focus <= 500 or 0 <= right_focus <= 500):
+            raise ValueError('Zoom value must be between 0 and 500.')
+
+        mot_l = self.motors[self.connector['left']]
+        mot_r = self.motors[self.connector['right']]
+        command = f'G1 {mot_l["focus"]}{left_focus} {mot_r["focus"]}{right_focus} F{self.speed}'
         self.ser.write(bytes(command + '\n', 'utf8'))
         _ = self.ser.readline()
 
